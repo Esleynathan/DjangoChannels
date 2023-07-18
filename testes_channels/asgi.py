8622cec070c49fc32bd459f8b4d4f5a6c234d46c
admin <esley@ubuntu.myguest.virtualbox.org>
2023-07-18 14:58:19 -0300

    Django Channels | 3 - Criando grupos

--- testes_channels/asgi.py
+++ testes_channels/asgi.py
@@ -1,11 +1,10 @@
 import os
-
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import meu_app.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'teste_channels.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testes_channels.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
--- testes_channels/settings.py
+++ testes_channels/settings.py
@@ -20,6 +20,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -60,6 +61,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'testes_channels.wsgi.application'
 ASGI_APPLICATION = "testes_channels.asgi.application"
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
 
 
 # Database
